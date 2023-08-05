0c6de7923b65c256510f5a17f671d58bce03fa5c
jimmy106 <pandabucchibecker@gmail.com>
2023-08-05 15:58:27 +0400

    Added Item (no texture yet)

--- src/main/java/net/qinx/lilcobble/LILCobblestone.java
+++ src/main/java/net/qinx/lilcobble/LILCobblestone.java
@@ -2,6 +2,7 @@ package net.qinx.lilcobble;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.qinx.lilcobble.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ public class LILCobblestone implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/qinx/lilcobble/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.qinx.lilcobble.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.qinx.lilcobble.LILCobblestone;
+
+public class ModItems {
+    public static final Item LILCOBBLESTONE = registerItem("lilcobblestone", new Item(new FabricItemSettings()));
+
+    private static void addItemsToBuildingBlocksItemGroup(FabricItemGroupEntries entries) {
+        entries.add(LILCOBBLESTONE);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(LILCobblestone.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        LILCobblestone.LOGGER.info("Registering Mod items for " + LILCobblestone.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(ModItems::addItemsToBuildingBlocksItemGroup);
+    }
+}
